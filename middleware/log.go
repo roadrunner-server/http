c70107a512ebaf267ac2ade73a7a90129feb6df1
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-06-23 00:11:18 +0200

    remove unused code
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- middleware/log.go
+++ middleware/log.go
@@ -16,9 +16,8 @@ type wrapper struct {
 	io.ReadCloser
 	read int
 
-	code      int
-	data      []byte
-	HeaderMap http.Header
+	code int
+	data []byte
 }
 
 func (w *wrapper) Read(b []byte) (int, error) {
@@ -31,26 +30,11 @@ func (w *wrapper) Close() error {
 	return w.ReadCloser.Close()
 }
 
-func (w *wrapper) Write(data []byte) (int, error) {
-	w.data = make([]byte, len(data))
-	copy(w.data, data)
-	return len(data), nil
-}
-
-func (w *wrapper) WriteHeader(code int) {
-	w.code = code
-}
-
-func (w *wrapper) Header() http.Header {
-	return w.HeaderMap
-}
-
 func (w *wrapper) reset() {
 	w.code = 0
 	w.read = 0
 	w.data = nil
 	w.ReadCloser = nil
-	w.HeaderMap = make(map[string][]string)
 }
 
 type lm struct {
@@ -73,42 +57,18 @@ func NewLogMiddleware(next http.Handler, accessLogs bool, log *zap.Logger) http.
 
 func (l *lm) Log(next http.Handler, accessLogs bool) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "PRI" {
-			next.ServeHTTP(w, r)
-			l.log.Info("upgrade", zap.String("method", "PRI"), zap.Any("headers", r.Header))
-			return
-		}
-
 		start := time.Now()
 
 		bw := l.getW()
 		defer l.putW(bw)
 
-		bw.HeaderMap = make(map[string][]string)
 		r2 := *r
 		if r2.Body != nil {
 			bw.ReadCloser = r2.Body
 			r2.Body = bw
 		}
 
-		next.ServeHTTP(bw, &r2)
-
-		// write headers back
-		for k, v := range bw.HeaderMap {
-			for i := 0; i < len(v); i++ {
-				w.Header().Add(k, v[i])
-			}
-		}
-
-		// write status code
-		w.WriteHeader(bw.code)
-		// write data
-		_, err := w.Write(bw.data)
-		if err != nil {
-			l.log.Error("write response", zap.Error(err))
-			return
-		}
-
+		next.ServeHTTP(w, &r2)
 		l.writeLog(accessLogs, r, bw, start)
 	})
 }
