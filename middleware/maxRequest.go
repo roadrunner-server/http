39d3c206b3decd3922faeffe4521e013bf7f7929
Valery Piashchynski <piashchynski.valery@gmail.com>
2023-06-06 16:23:10 +0200

    chore: update deps, update maxRequest logic
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- middleware/maxRequest.go
+++ middleware/maxRequest.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 )
 
@@ -8,10 +9,10 @@ func MaxRequestSize(next http.Handler, maxReqSize uint64) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// validating request size
 
-		r2 := *r
+		r2 := r.Clone(context.Background())
 		r2.Body = http.MaxBytesReader(w, r2.Body, int64(maxReqSize))
 
 		// use max_request_size limit in megabytes
-		next.ServeHTTP(w, &r2)
+		next.ServeHTTP(w, r2)
 	})
 }
