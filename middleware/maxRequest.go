6bb4c5f434d0d27ca90e01eeab8281788cf935c6
Valery Piashchynski <piashchynski.valery@gmail.com>
2023-06-08 15:47:51 +0200

    fix: do not overwrite http context
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- middleware/maxRequest.go
+++ middleware/maxRequest.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"context"
 	"net/http"
 )
 
@@ -9,7 +8,7 @@ func MaxRequestSize(next http.Handler, maxReqSize uint64) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// validating request size
 
-		r2 := r.Clone(context.Background())
+		r2 := r.Clone(r.Context())
 		r2.Body = http.MaxBytesReader(w, r2.Body, int64(maxReqSize))
 
 		// use max_request_size limit in megabytes
