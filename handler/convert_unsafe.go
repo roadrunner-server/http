6c24436f43b90afea2387cd283552e849e510f6a
Valery Piashchynski <piashchynski.valery@gmail.com>
2023-07-13 16:52:43 +0200

    chore: 2x faster postform, multipartform processing
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ handler/convert_unsafe.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"unsafe"
+)
+
+func bytesToStr(data []byte) string {
+	if len(data) == 0 {
+		return ""
+	}
+
+	return unsafe.String(unsafe.SliceData(data), len(data))
+}
+
+func strToBytes(data string) []byte {
+	if data == "" {
+		return nil
+	}
+
+	return unsafe.Slice(unsafe.StringData(data), len(data))
+}
